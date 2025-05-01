f7d5cb0001639332a9c96c6eda4dbf2dfa7d8c9d
HenryCa <henrycauan3222@gmail.com>
2025-04-30 22:29:02 -0300

    :first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <input type="number" defaultValue={0} />
       </body>
     </html>
   );
